cec432efac2f43cf87b0c20cb9ec709bcc67bfc1
MistyIslam5 <mdmisty5@gmail.com>
2024-08-29 20:53:05 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// console.log(5 < 12);
+// console.log(5 > 12);
+// console.log(5 == 12);
+// console.log(12 == 12);
+console.log(16 >= 12);
+console.log(5 <= 12);
+
+
+var applePrice = 450;
+var orangePrice = 299;
+console.log(applePrice < applePrice);
+
+
+console.log(100 == '100');
+console.log(100 === '100');  
+
+
