fca2ee1200ac98c5e60ff263d1756dbec0b5d607
MistyIslam5 <mdmisty5@gmail.com>
2024-08-30 11:36:42 +0600

    task 5 done

---
+++ task5.js
@@ -0,0 +1,14 @@
+var num1 = 40;
+var num2 = 30;
+
+// if (num1 > num2) {
+//     const num3 = num1 + num1;
+//     console.log(num3);
+    
+// }else{
+//     const num3 = num1 + num2;
+//     console.log(num3);
+// }
+
+let num3 = num1 > num2 ? num1 + num1 : num1 + num2;
+console.log(num3);
